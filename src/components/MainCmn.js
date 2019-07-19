f95636ccbc9da73bc33c9b4f11a032aab13c5b22
PhongDo <phong.do@fssc.com.vn>
2019-07-19 08:44:21 +0700

    cmt

--- src/components/MainCmn.js
+++ src/components/MainCmn.js
@@ -1,37 +1,33 @@
 import React, { Component } from 'react';
 import BuyDT from '../data/BuyDT'
 import SellDT from '../data/SellDT'
+import { throwStatement } from '@babel/types';
+import { async } from 'q';
 class MainCmn extends Component {
     constructor(props) {
         super(props);
         this.state = {
             results:[],
-            change: true
+            change: true,
+            BuyDT: [],
+            SellDT: []
         }
     }
-    caculating(SellDT) {
-        var sellLen = SellDT.length
-        var index = 0
-        while (index<sellLen) {
-            var id = index
-            var symbol =SellDT[index].symbol 
-            var sellArr = []
-            var buyArr = []
-            while (id<sellLen) {
-                if (SellDT[index].symbol!==SellDT[id].symbol) break
-                id++
-            }
-            sellArr = SellDT.slice(index, id)
-            index = id
-            console.log(sellArr)
-        }
+    async changeBuyDT() {
+        this.setState({
+            BuyDT: BuyDT
+        });
     }
-    cutArray
+    async changeSellDT() {
+        this.setState({
+            SellDT: SellDT
+        });
+    }
     processing(SellDT, BuyDT) {
-        while (SellDT[0].txdate<BuyDT[0].txdate) {
+        while ((SellDT.length>0)&&(BuyDT.length>0)&&(SellDT[0].txdate<BuyDT[0].txdate)) {
             SellDT.shift()
         }
-        var result = []
+        var results = []
         var obj = {}
         while (SellDT.length > 0) {
             if (SellDT[0].orderqtty <= BuyDT[0].orderqtty) {
@@ -39,7 +35,7 @@ class MainCmn extends Component {
                 obj = {
                     ...SellDT[0]
                 }
-                obj.buyingPrice = BuyDT[0].oderamt
+                obj.buyingPrice = BuyDT[0].orderamt
                 BuyDT[0].orderqtty -= SellDT[0].orderqtty
                 if (BuyDT[0].orderqtty===0) BuyDT.shift()
                 SellDT.shift()
@@ -50,21 +46,27 @@ class MainCmn extends Component {
                     ...SellDT[0]
                 }
                 obj.orderqtty = BuyDT[0].orderqtty
-                obj.buyingPrice = BuyDT[0].oderamt
+                obj.buyingPrice = BuyDT[0].orderamt
                 SellDT[0].orderqtty -= BuyDT[0].orderqtty
                 BuyDT.shift()
             }
-            result.push(obj)
+            results.push(obj)
         }
-        console.log('result: ', result)
+        this.setState({
+            results: results
+        });
     }
-    
+    async clickCal() {
+        await this.changeBuyDT()
+        this.changeSellDT()
+        this.processing(this.state.SellDT, this.state.BuyDT)
+    }
     render() {
-        {this.caculating(SellDT)}
+        console.log('results: ', this.state.results);
+        
         return (
             <div>
-                ReactJs
-
+                <div className="btn btn-success" onClick={()=>this.clickCal()}>Caculating</div>
             </div>
         );
     }
